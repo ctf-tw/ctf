56d1b0d5924cd103ca54fa8678472a8d65071a45
JonasTuhte <jonas.tuhte@transferwise.com>
2017-11-19 00:30:11 +0800

    Added functionality to upload bad users

--- dbQuery.R
+++ dbQuery.R
@@ -10,8 +10,28 @@ setUpConnection <- function(){
 
 
 getData <- function(tableName){
-  query <- gsub("TABLENAME", tableName, "SELECT * FROM TABLENAME")
+  query <- gsub("TABLENAME", tableName, "SELECT * FROM TABLENAME;")
   con <- setUpConnection()
   data <- dbGetQuery(con, query)
+  dbDisconnect(con)
   return(data)
 }
+
+
+saveResults <- function(users, descriptions){
+  con <- setUpConnection()
+  dbGetQuery(con, "CREATE TABLE IF NOT EXISTS XXXXX (
+             id_user INT,
+             description TEXT);")
+  
+  for(i in length(users)){
+    query <- "INSERT INTO XXXXX
+              ID_USER,
+              DESCRIPTION;"
+    query <- gsub("ID_USER", users[i], query)
+    query <- gsub("DESCRIPTION", descriptions[i], query)
+    dbGetQuery(con, query)
+  }
+  dbDisconnect(con)
+}
+
