94bb596ef0b23461d2323681dd3324d046a74710
Vadym Vasyliev <vadim@transferwise.com>
2017-11-18 23:21:13 +0800

    Added bad users polling call

--- src/main/java/kl/tw/ctf/web/rest/BadUserResource.java
+++ src/main/java/kl/tw/ctf/web/rest/BadUserResource.java
@@ -1,5 +1,7 @@
 package kl.tw.ctf.web.rest;
 
+import kl.tw.ctf.domain.SuspectedReason;
+import kl.tw.ctf.repository.SuspectedReasonRepository;
 import kl.tw.ctf.service.RLauncherService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,33 +9,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/management")
 public class BadUserResource {
     private final Logger log = LoggerFactory.getLogger(BadUserResource.class);
 
+    @Autowired
+    SuspectedReasonRepository suspectedReasonRepository;
+
     @PostMapping("/badUserList")
-    public ResponseEntity<?> showBadUsers(@RequestParam("id") String id) {
+    public @ResponseBody List<SuspectedReason> showBadUsers(@RequestParam("id") String id) {
         log.debug("Graph requested!");
-        String result = null;
+        List<SuspectedReason> result;
 
         if (id == null) {
             log.debug("No id provided!");
-            return new ResponseEntity("please select a file!", HttpStatus.FORBIDDEN);
+            return null;
         }
 
         try {
-           //DB ops
+            result = suspectedReasonRepository.findAll();
+
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            log.error("Can't load reasons" + e.getMessage());
+            return null;
         }
 
-        return new ResponseEntity(result, new HttpHeaders(), HttpStatus.OK);
+        return result   ;
 
     }
 
