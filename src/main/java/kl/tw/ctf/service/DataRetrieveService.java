5b132c1a4b9acea8a75898e8338312443ad1f02c
ivan.lissitsnoi <ivan.lissitsnoi@transferwise.com>
2017-11-19 10:31:05 +0200

    linking logic v1

--- src/main/java/kl/tw/ctf/service/DataRetrieveService.java
+++ src/main/java/kl/tw/ctf/service/DataRetrieveService.java
@@ -1,16 +1,23 @@
 package kl.tw.ctf.service;
 
 import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.*;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.gson.Gson;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 import kl.tw.ctf.dao.DataFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -35,98 +42,157 @@ public class DataRetrieveService {
 
         List<Map<String, Object>> rowResults = jdbcTemplate.queryForList(query);
 
-        Map<String, List<Object>> userData = new HashMap<>();
+        Set<Node> userNodes = new HashSet<>();
+        addNodesToSet(rowResults, userNodes);
 
-        rowResults.forEach(row -> row.entrySet().forEach((entry -> {
-            if (userData.containsKey(entry.getKey())) {
-                userData.get(entry.getKey()).add(entry.getValue());
-            } else {
-                List<Object> values = new ArrayList<>();
-                values.add(entry.getValue());
-                userData.put(entry.getKey(), values);
-            }
-        })));
-
-        userData.remove("user_id");
-        return buildGraphForData(userData, dataFile.getTableName(), id);
+        return buildGraphForData(userNodes, dataFile);
     }
 
-    private String buildGraphForData(Map<String, List<Object>> userData, String tableName, String userId) {
-        Map<String, List<MatchHolder>> typeToMatchedUsers = new HashMap<>();
-        userData.entrySet().forEach(entry -> {
-            String column = entry.getKey();
+    private String buildGraphForData(Set<Node> nodes, DataFile dataFile) {
+        String columns = String.join(",", dataFile.getColumnNames());
+        String condition = ("SELECT " + columns + " FROM " + dataFile.getTableName()) + " WHERE " + " ";
+        for (String columnName : dataFile.getColumnNames()) {
+            String preparation =
+                "('" + String
+                    .join("','", nodes.stream().filter(el -> el.type.equals(columnName)).map(el -> el.value).collect(
+                        toList())) + "')";
+            String prep = columnName + " in " + preparation + " OR ";
+            condition += prep;
+        }
+        condition = condition.substring(0, condition.length() - 3);
 
-            entry.getValue().forEach(value -> {
-                String sql =
-                    "SELECT user_id FROM " + tableName
-                        + " WHERE " + column + " = (\'" + value.toString() + "\') "
-                        + "AND user_id <> \'" + userId + "\'";
-                List<String> userIds = jdbcTemplate.query(sql, (rs, row) -> rs.getString("user_id"));
-                if (!userIds.isEmpty()) {
-                    if (typeToMatchedUsers.containsKey(column)) {
-                        typeToMatchedUsers.get(column).add(new MatchHolder(value.toString(), userIds));
-                    } else {
-                        List<MatchHolder> matchHolders = new ArrayList<>();
-                        matchHolders.add(new MatchHolder(value.toString(), userIds));
-                        typeToMatchedUsers.put(column, matchHolders);
-                    }
+        List<Map<String, Object>> rowResults = jdbcTemplate.queryForList(condition);
+        addNodesToSet(rowResults, nodes);
+
+        HashMap<String, List<Node>> map = new HashMap<>();
+        nodes.forEach(node -> {
+            String key = node.type + " | " +  node.value;
+            if (map.containsKey(key)) {
+                map.get(key).add(node);
+            }
+            else {
+                List<Node> nodes1 = new ArrayList<>();
+                nodes1.add(node);
+                map.put(key, nodes1);
+            }
+        });
+
+        Set<Edge> edges = convertToEdges(map);
+
+        return convertToJson(selectMatchedNodes(edges, nodes), edges);
+    }
+
+
+    private Set<Node> selectMatchedNodes(Set<Edge> edges, Set<Node> nodes) {
+        Set<Node> results = new HashSet<>();
+        edges.forEach(edge -> {
+            nodes.forEach(node -> {
+                if (Objects.equals(node.uniqueId(), edge.target) || Objects.equals(node.uniqueId(), edge.source)) {
+                    results.add(node);
                 }
             });
-
         });
-        return convertToJsonResponse(userData, typeToMatchedUsers);
+        return results;
     }
 
-    private String convertToJsonResponse(Map<String, List<Object>> userTypeToValues, Map<String, List<MatchHolder>> matchedUsers) {
-        Gson gson = new Gson();
-
-        List<Node> nodes = new ArrayList<>();
-        userTypeToValues.entrySet().forEach(entry -> {
-            entry.getValue().forEach(value -> {
-                String type = entry.getKey();
-                nodes.add(new Node((type + value.toString()).hashCode(), value.toString()));
-            });
-        });
-
-        List<Edge> edges = new ArrayList<>();
-
-        matchedUsers.entrySet().forEach(entry -> {
-            entry.getValue().forEach(matchHolder -> {
-                matchHolder.userIds.forEach(userTargetId -> {
-                    String type = entry.getKey();
-                    edges.add(new Edge((type + matchHolder.matchedValue).hashCode(), Integer.parseInt(userTargetId), matchHolder.matchedValue));
+    private Set<Edge> convertToEdges(Map<String, List<Node>> dataMap) {
+        Set<Edge> edges = new HashSet<>();
+        dataMap.entrySet().forEach(entry -> {
+            if (entry.getValue().size() > 1) {
+                List<Node> nodes = entry.getValue();
+                nodes.forEach(node -> {
+                    String key1 = node.value + node.type;
+                    String key2 = node.id;
+                    edges.add(new Edge(key1, key2, "1"));
                 });
-
-            });
+            }
         });
+        return edges;
+    }
 
-        matchedUsers.values().stream().flatMap(Collection::stream)
-            .map(matchHolder -> matchHolder.userIds).flatMap(Collection::stream)
-            .collect(toSet())
-            .stream().forEach(id -> nodes.add(new Node(Integer.parseInt(id), "user")));
+    private void addNodesToSet(List<Map<String, Object>> rowResults, Set<Node> nodes) {
+        rowResults.forEach(row -> {
+            String  currentUserId = null;
+            int count = 0;
+            for (Map.Entry<String, Object> entry1: row.entrySet()) {
+                Node node;
+                if (count == 0) {
+                    count++;
+                    currentUserId = entry1.getValue().toString();
+                    String type = entry1.getKey();
+                    node = new Node(currentUserId, type, currentUserId);
+                }
+                else {
+                    String type = entry1.getKey();
+                    String value1 = entry1.getValue().toString();
+                    node = new Node(currentUserId, type, value1);
+                }
+                nodes.add(node);
+            }
+        });
+    }
+
+    private String convertToJson(Set<Node> nodes, Set<Edge> edges) {
+        Gson gson = new Gson();
+        Set<RenderNode> nodesToRender = nodes.stream().map(node -> new RenderNode(node.uniqueId(), node.type, node.value))
+            .collect(toSet());
+
+        Graph graph = new Graph(nodesToRender, edges);
+        return gson.toJson(graph);
 
 
-        return gson.toJson(new Graph(nodes, edges));
+    }
 
+    static class RenderNode {
+        String id;
+        String type;
+        String value;
+
+        public RenderNode(String id, String type, String value) {
+            this.id = id;
+            this.type = type;
+            this.value = value;
+        }
     }
 
     static class Graph {
-        List<Node> nodes;
-        List<Edge> edges;
+        Set<RenderNode> nodes;
+        Set<Edge> edges;
 
-        public Graph(List<Node> nodes, List<Edge> edges) {
+        public Graph(Set<RenderNode> nodes, Set<Edge> edges) {
             this.nodes = nodes;
             this.edges = edges;
         }
     }
 
     static class Node {
-        Integer id;
+        String id;
         String type;
+        String value;
 
-        public Node (Integer id, String type) {
+        public Node (String id, String type, String value) {
             this.id = id;
             this.type = type;
+            this.value = value;
+        }
+
+        public String uniqueId() {
+            if (Objects.equals(type, "user_id")) {
+                return id;
+            }
+            return value + type;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Node)
+                return ((Node) obj).uniqueId().equals(this.uniqueId()) && ((Node) obj).type.equals(this.type) && ((Node) obj).value.equals(this.value);
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return (this.id).hashCode() + (this.value).hashCode() * 32 + (this.type).hashCode() * 32 * 32 ;
         }
 
         @Override
@@ -136,11 +202,11 @@ public class DataRetrieveService {
     }
 
     static class Edge {
-        Integer source;
-        Integer target;
+        String source;
+        String target;
         String value;
 
-        public Edge(Integer source, Integer target, String value) {
+        public Edge(String source, String target, String value) {
             this.source = source;
             this.target = target;
             this.value = value;
